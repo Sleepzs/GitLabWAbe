96860f0d22389c4008181cf2d04fb7adb49c1cde
Sleepzs <166639475+Sleepzs@users.noreply.github.com>
2024-10-02 10:24:28 -0400

    good enough

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.githubwabe {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.githubwabe to javafx.fxml;
-    exports org.example.githubwabe;
-}
